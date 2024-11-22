6d51747a7b84009f127088448713a5ec6705c820
Kit086 <kit.j.lau@outlook.com>
2024-11-22 10:37:00 +0800

    feat: 调整 configure 带宽参数，由 kbps 调整为 kbit，且更新 README 中的使用说明。另外 README 添加 `如何观察测试结果` 部分，简单记录测试方法。

--- configure.sh
+++ configure.sh
@@ -6,8 +6,8 @@ SRC=""                 # 源地址
 DST=""                 # 目标地址
 UP_DELAY="100ms"       # 上行延迟
 DOWN_DELAY="100ms"     # 下行延迟
-UP_BANDWIDTH="128kbps" # 上行带宽
-DOWN_BANDWIDTH="512kbps" # 下行带宽
+UP_BANDWIDTH="128kbit" # 上行带宽
+DOWN_BANDWIDTH="512kbit" # 下行带宽
 LOSS="0%"              # 丢包率
 JITTER="0ms"           # 抖动
 
@@ -46,7 +46,7 @@ sudo ip link set ifb0 up || { echo "无法启用 ifb0 接口，请检查 ifb 模
 echo "配置上行规则（船舶 -> 卫星）..."
 sudo iptables -t mangle -A OUTPUT -s "$SRC" -d "$DST" -j MARK --set-mark 1
 sudo tc qdisc add dev "$IFACE" root handle 1: htb
-sudo tc class add dev "$IFACE" parent 1: classid 1:1 htb rate "$UP_BANDWIDTH"
+sudo tc class add dev "$IFACE" parent 1: classid 1:1 htb rate "$UP_BANDWIDTH" ceil "$UP_BANDWIDTH"
 sudo tc qdisc add dev "$IFACE" parent 1:1 handle 10: netem delay "$UP_DELAY" "$JITTER" loss "$LOSS"
 sudo tc filter add dev "$IFACE" protocol ip parent 1:0 prio 1 handle 1 fw flowid 1:1
 
@@ -56,7 +56,7 @@ sudo iptables -t mangle -A PREROUTING -s "$DST" -d "$SRC" -j MARK --set-mark 2
 sudo tc qdisc add dev "$IFACE" ingress
 sudo tc filter add dev "$IFACE" parent ffff: protocol ip handle 2 fw action mirred egress redirect dev ifb0
 sudo tc qdisc add dev ifb0 root handle 20: htb
-sudo tc class add dev ifb0 parent 20: classid 20:1 htb rate "$DOWN_BANDWIDTH"
+sudo tc class add dev ifb0 parent 20: classid 20:1 htb rate "$DOWN_BANDWIDTH" ceil "$DOWN_BANDWIDTH"
 sudo tc qdisc add dev ifb0 parent 20:1 handle 30: netem delay "$DOWN_DELAY" "$JITTER" loss "$LOSS"
 sudo tc filter add dev ifb0 protocol ip parent 20:0 prio 1 handle 2 fw flowid 20:1
 
